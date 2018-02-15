26d805196ff0e7fea1f07e81c8aca9d0920dd0d2
Robert Schilling <rschilling@student.tugraz.at>
2018-02-15 13:57:56 +0100

    Add unique index on protected_tags:[:project_id, :name]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208183958) do
+ActiveRecord::Schema.define(version: 20180215124300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1504,7 +1504,7 @@ ActiveRecord::Schema.define(version: 20180208183958) do
     t.datetime "updated_at"
   end
 
-  add_index "protected_branches", ["project_id"], name: "index_protected_branches_on_project_id", using: :btree
+  add_index "protected_tags", ["project_id", "name"], name: "index_protected_tags_on_project_id_and_name", unique: true, using: :btree
 
   create_table "protected_tag_create_access_levels", force: :cascade do |t|
     t.integer "protected_tag_id", null: false
