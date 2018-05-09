b06837c619b9bb3e94d0237a2070653e25cb13c6
blackst0ne <blackst0ne.ru@gmail.com>
2018-05-10 09:54:09 +1100

    Refactor `user_creates_merge_request_spec.rb`

--- spec/features/projects/merge_requests/user_creates_merge_request_spec.rb
+++ spec/features/projects/merge_requests/user_creates_merge_request_spec.rb
@@ -1,6 +1,6 @@
-require 'spec_helper'
+require "spec_helper"
 
-describe 'User creates a merge request', :js do
+describe "User creates a merge request", :js do
   let(:project) { create(:project, :repository) }
   let(:user) { create(:user) }
 
@@ -11,22 +11,22 @@ describe 'User creates a merge request', :js do
     visit(project_new_merge_request_path(project))
   end
 
-  it 'creates a merge request' do
-    find('.js-source-branch').click
-    click_link('fix')
+  it "creates a merge request" do
+    find(".js-source-branch").click
+    click_link("fix")
 
-    find('.js-target-branch').click
-    click_link('feature')
+    find(".js-target-branch").click
+    click_link("feature")
 
-    click_button('Compare branches')
+    click_button("Compare branches")
 
-    fill_in('merge_request_title', with: 'Wiki Feature')
-    click_button('Submit merge request')
+    TITLE = "Wiki Feature".freeze
 
-    page.within('.merge-request') do
-      expect(page).to have_content('Wiki Feature')
+    fill_in("Title", with: TITLE)
+    click_button("Submit merge request")
+
+    page.within(".merge-request") do
+      expect(page).to have_content(TITLE)
     end
-
-    wait_for_requests
   end
 end
