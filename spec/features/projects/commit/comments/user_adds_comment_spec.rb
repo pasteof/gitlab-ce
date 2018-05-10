3d8499a726e689a66b0bdbac4752528ba0a14bdf
blackst0ne <blackst0ne.ru@gmail.com>
2018-05-10 13:16:59 +1100

    Refactor current specs

---
+++ spec/features/projects/commit/comments/user_adds_comment_spec.rb
@@ -0,0 +1,53 @@
+require "spec_helper"
+
+describe "User adds a comment on a commit", :js do
+  include Spec::Support::Helpers::Features::NotesHelpers
+  include RepoHelpers
+
+  let(:comment_text) { "XML attached" }
+  let(:project) { create(:project, :repository) }
+  let(:user) { create(:user) }
+
+  before do
+    sign_in(user)
+    project.add_developer(user)
+
+    visit(project_commit_path(project, sample_commit.id))
+  end
+
+  it "adds a comment" do
+    EMOJI = ":+1:".freeze
+
+    page.within(".js-main-target-form") do
+      expect(page).not_to have_link("Cancel")
+
+      fill_in("note[note]", with: "#{comment_text} #{EMOJI}")
+
+      # Check on `Preview` tab
+      click_link("Preview")
+
+      expect(find(".js-md-preview")).to have_content(comment_text).and have_css("gl-emoji")
+      expect(page).not_to have_css(".js-note-text")
+
+      # Check on the `Write` tab
+      click_link("Write")
+
+      expect(page).to have_field("note[note]", with: "#{comment_text} #{EMOJI}")
+
+      # Submit comment from the `Preview` tab to get rid of a separate `it` block
+      # which would specially tests if everything gets cleared from the note form.
+      click_link("Preview")
+      click_button("Comment")
+    end
+
+    wait_for_requests
+
+    page.within(".note") do
+      expect(page).to have_content(comment_text).and have_css("gl-emoji")
+    end
+
+    page.within(".js-main-target-form") do
+      expect(page).to have_field("note[note]", with: "").and have_no_css(".js-md-preview")
+    end
+  end
+end
