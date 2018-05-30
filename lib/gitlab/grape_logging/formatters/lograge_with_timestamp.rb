1d45f44d76e03466ec40db25286e03103fc7c381
Imre Farkas <ifarkas@gitlab.com>
2018-05-30 12:12:42 +0200

    Enforce UTF8 encoding on user input in LogrageWithTimestamp formatter

--- lib/gitlab/grape_logging/formatters/lograge_with_timestamp.rb
+++ lib/gitlab/grape_logging/formatters/lograge_with_timestamp.rb
@@ -2,8 +2,12 @@ module Gitlab
   module GrapeLogging
     module Formatters
       class LogrageWithTimestamp
+        include Gitlab::EncodingHelper
+
         def call(severity, datetime, _, data)
           time = data.delete :time
+          utf8_encode_values(data[:params]) if data.has_key?(:params)
+
           attributes = {
             time: datetime.utc.iso8601(3),
             severity: severity,
@@ -12,6 +16,19 @@ module Gitlab
             view: time[:view]
           }.merge(data)
           ::Lograge.formatter.call(attributes) + "\n"
+        end
+
+        private
+
+        def utf8_encode_values(data)
+          case data
+          when Hash
+            data.values.each { |v| utf8_encode_values(v) }
+          when Array
+            data.each { |v| utf8_encode_values(v) }
+          when String
+            encode_utf8 data
+          end
         end
       end
     end
